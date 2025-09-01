c8a4a510ec6eeab437a191c6a2c557f5c797bb26
dewansh-iship <dewansh@ishipplus.com>
2025-09-01 21:24:32 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'https://ship-inspect.onrender.com',
+  baseURL: 'https://ship-inspect-1.onrender.com',
   timeout: 0,  // 2 minutes
   withCredentials: true, // IMPORTANT
 });
